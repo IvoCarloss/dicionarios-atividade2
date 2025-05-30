4fbcc6d618ce6cf0f52af47a73e52091184a3e0b
IvoCarloss <ivoc4337@gmail.com>
2025-05-30 08:27:37 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -14,23 +14,23 @@ busca = input("Informe o nome do produto que deseja pesquisar: ")
 if busca in estoque:
     print(f"A quantidade do produto {busca} é {estoque[busca]} ")
 
-    po = int(input("Você quer atualizar seu pedido? (1 para adição / 2 para remoção): "))
+    po = int(input("Você quer atualizar seu pedido? 1  adição 2 remoção: "))
 
     if po == 1:
         c = int(input(f"Quanto você quer adicionar no seu pedido de {busca}? "))
         estoque[busca] += c
-        print(f"Resultado da adição: agora temos {estoque[busca]} unidades de {busca}.")
+        print(f"Resultado da adição agora temos {estoque[busca]} unidades de {busca}")
 
     elif po == 2:
         c1 = int(input(f"Quanto você quer remover do seu pedido de {busca}? "))
         if c1 <= estoque[busca]:
             estoque[busca] -= c1
-            print(f"Resultado da remoção: agora temos {estoque[busca]} unidades de {busca}.")
+            print(f"Resultado da remoção: agora temos {estoque[busca]} unidades de {busca}")
         else:
-            print("Erro: você tentou remover mais do que tem no estoque!")
+            print("Erro você tentou remover mais do que tem no estoque")
 
     else:
-        print("Opção inválida!")
+        print("Opção inválida")
 
 else:
-    print("O produto não está no estoque!")
+    print("O produto não está no estoque")
