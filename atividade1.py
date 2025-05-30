ce5334db8aec02df9150e4b354c07dadb5523183
IvoCarloss <ivoc4337@gmail.com>
2025-05-30 08:28:22 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -4,20 +4,19 @@ boletim = {}
 qtd = int(input("Quantos alunos deseja cadastrar? "))
 for _ in range(qtd):
     nome = input("Nome do aluno: ")
-    nota = float(input(f"Nota final de {nome} (0 a 10): "))
+    nota = float(input(f"Nota final de {nome} 0 a 10: "))
     boletim[nome] = nota
 
 
-print("\nBoletim:")
+print("Boletim:")
 for nome, nota in boletim.items():
     print(f"{nome}: {nota}")
 
 while True:
-    busca = input("\nConsultar nota de qual aluno? (ou 'sair' para encerrar): ")
+    busca = input("Consultar nota de qual aluno? ou 'sair': ")
     if busca.lower() == 'sair':
         break
     elif busca in boletim:
         print(f"Nota de {busca}: {boletim[busca]}")
     else:
-        print("Aluno não encontrado.")
-
+        print("Aluno não encontrado")
